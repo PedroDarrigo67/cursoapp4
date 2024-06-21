77da6ea0956c4c68a9bb7efba0a9a7e124e18618
PedroDarrigo67 <pedrodarrigo@gmail.com>
2024-06-21 11:05:14 -0300

    proyecto incompleto

---
+++ screens/RecentExpense.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function RecentExpense() {
+  return (
+    <Text>
+        RecentExpense
+    </Text>
+  )
+}
+
+export default RecentExpense
